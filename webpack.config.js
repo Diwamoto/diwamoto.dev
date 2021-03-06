a0370ba8404a70a18dc1b4622826cfcc92295136
Diwamoto <daiki.work.0118@gmail.com>
2021-03-06 23:05:33 +0900

    開発環境を準備

---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+const PATH = require('path');
+const SCSS_PATH = PATH.join(__dirname, './src/scss');
+const CSS_PATH = PATH.join(__dirname, './src/css');
+const TS_PATH = PATH.join(__dirname, './src/ts');
+const JS_PATH = PATH.join(__dirname, './src/js');
