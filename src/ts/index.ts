5866319a34077e993089cb725cc4c805f6559bb2
Diwamoto <daiki.work.0118@gmail.com>
2021-03-19 00:11:21 +0900

    フッター作った/フォントいじった/もろもろ調整

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,3 +3,8 @@ import '/src/img/apple-touch-icon.png';
 import '/src/img/ogp.png';
 import '/src/img/profile_icon.jpg';
 import '/src/img/top.jpeg';
+import '/src/img/links/facebook.png';
+import '/src/img/links/github.png';
+import '/src/img/links/mail.png';
+import '/src/img/links/twitter.png';
+import '/src/img/links/zenn.png';
